fe29e10ce8464ea209f88aaba73a56420eeed117
wee08 <manutnithyasean@gmail.com>
2026-02-25 20:53:43 +0700

    Add function to find roots of quadratic equations and refactor main menu for better user interaction

--- TP/TP1/Ex8.cpp
+++ TP/TP1/Ex8.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <math.h>
+#include <iomanip>
 using namespace std;
 
 void ConvertCelsiusToFahrenhite(){
@@ -19,21 +21,62 @@ void ConvertFahrenhiteToCelsius(){
     cout  << cel << " C" << endl;
 }
 
+void FindRootOfQuadraticEqs(){
+    cout << "Find root of quadratic equation." << endl;
+    int a,b,c;
+    cout << "Enter a, b, c: ";
+    cin >> a >> b >> c;
+    int delta = pow(b,2) -( 4 * a * c);
+    float root_1,root_2;
+
+    if(delta > 0){
+        cout << "Delta > 0" << endl;
+        root_1 = ((-pow(b,2) + sqrt(delta))) /( 2*a);
+        root_2 = ((-pow(b,2) - sqrt(delta))) /( 2*a);
+        cout << "x1 = " << setprecision(2) << root_1 << endl << "x2 = " << root_2 << endl;
+    }else if(delta == 0){
+        cout << "Delta = 0" << endl;
+        root_1 = -(pow(b,2)) / ( 2*a);
+        root_2 = root_1;
+        cout << setprecision(2) <<"x1 = x2 = " << root_1  << endl;
+    }else {
+        delta = -1 * delta;
+        float imaPart = (sqrt(delta)) / (2 * a);
+        float realPart = -(pow(b,2)) / ( 2 * a);
+
+        cout << "delta < 0" << endl;
+        cout << "x1 = " << setprecision(2) << realPart <<" + "<< imaPart << "i" <<endl;
+        cout << "x2 = " << setprecision(2) << realPart <<"-"<< imaPart << "i" <<endl;
+    }
+}
+
 int main(){
 
     int feature;
-
-    cout << endl << "(1) Convert celsius to fahrenhite." << endl;
-    cout << "(2) Convert fahrenhite to celsius." << endl;
-    cout << "(3) Find root of quadratic equation." << endl;
-    cout << "(4) Compute BMI." << endl;
-    cout << "(5) Sum numbers from 1 to n." << endl;
-    cin >> feature;
-    if(feature == 1){
-        ConvertCelsiusToFahrenhite();
-    }else if(feature == 2){
-        ConvertFahrenhiteToCelsius();
+    while (true)
+    {
+        cout << endl << "(1) Convert celsius to fahrenhite." << endl;
+        cout << "(2) Convert fahrenhite to celsius." << endl;
+        cout << "(3) Find root of quadratic equation." << endl;
+        cout << "(4) Compute BMI." << endl;
+        cout << "(5) Sum numbers from 1 to n." << endl;
+        cout << "Enter your choice:: " ;
+        cin >> feature;
+        switch(feature){
+            case 1:
+                ConvertCelsiusToFahrenhite();
+                break;
+            case 2:
+                ConvertFahrenhiteToCelsius();
+                break;
+            case 3:
+                FindRootOfQuadraticEqs();
+                break;
+            default:
+                cout << "INVALID CHOICE...!" << endl;
+                break;
+        }
     }
-    
+   
     return 0;
 }
--- TP/TP1/test.cpp
+++ TP/TP1/test.cpp
@@ -1,121 +1,22 @@
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <cstring>
+#include <complex>
+#include <cmath>
 
-using namespace std; // directive that allows to all elements from std (cout, )
+using namespace std;
 
 int main() {
-    cout <<"hello" << endl;
-    int a;
-    // cin >> a;
-    cout << a << endl;
+    double a, b, c;
+    cout << "Enter a, b, c: ";
+    cin >> a >> b >> c;
 
-    char name[20] ="manut";
-    strcpy(name, "dara") ;
-    cout << name << endl;
+    double delta = b*b - 4*a*c;
 
-    string my_name = "SEAN Manutnithya";
-    my_name = "SEAN MANUTNITHYA";
-    cout << my_name << endl;
+    complex<double> sqrtDelta = sqrt(complex<double>(delta, 0));
+    complex<double> x1 = (-b + sqrtDelta) / (2.0 * a);
+    complex<double> x2 = (-b - sqrtDelta) / (2.0 * a);
 
-    char name1[10] = "me";
-    string name2 = "me";
-    
-    if(name2 == name1){
-        cout << 1 + 1 << endl;
-    }
-    
-    bool isOpen = false;
-    if(name2 == name1) isOpen = true;
-
-    // won't run if fasle condition
-    cout << isOpen << endl ;
-
-    // name2.empty(); // check is there anything the variable or not
-
-    cout << 4 % 2 << endl;
-    if(isOpen){
-        cout << 1 + 10 << endl;
-    }
-
-    string emp;
-    if(emp.empty()) isOpen = true;
-    if(!isOpen){
-        cout << "add some value" << endl;
-    }else{
-        cout <<"well done" << endl;
-    }
-    cout << isOpen << endl;
-
-    // post increment
-    int b = 10;
-    int result1 = b++;
-    cout << b << endl;
-
-    // pre increment
-    int c = 10;
-    int result2 = c++; // 10 is called pre-increment mean it resigned the result2 before it does math
-    int result3 = ++c; // 11
-    cout << result2 << endl;// 10;
-    cout << c << endl;// 11
-
-
-    int m = 100;
-    int n = 1000;
-
-    bool isBigger = m < n;
-    cout << isBigger << endl;
-    if(isBigger) {cout <<  "true" << endl;
-    }else {
-        cout << "false" << endl;
-    }
-
-    // Cpp rendom number
-    int rendom;
-    srand(time(0));
-    rendom = rand() % (100 - 10+1) +1;
-    cout << rendom << endl;
-
-
-    // last character of string
-    string testname ="manutnithya";
-    cout << testname[testname.length() - 1] << endl;
-
-    int num_list[5] = {1,2,3,4,5};
-
-    for (int i = 0; i < 5; i++){
-        cout << num_list[i] << endl;
-        cout << num_list[i] << "\t";
-    }
-    // lenght of integer arrays
-    cout << sizeof(num_list) / sizeof(num_list[0])<< endl;
-    
-    // for loop
-    int a1 = 10;
-    int arr[4] ={1,2,3,5};
-    // a1 have the amount of step of arr length
-    // a1 = 4 -> 0 - 3
-    for(int a1 : arr)cout << "text" << a1 << endl;
-    
-    char spe = '#';
-    for (int st = 0; st < 1000; st++){
-        if(st % 2 == 0){
-            cout << spe << "  ";
-        }
-        cout << spe << "";
-    }
-
-
-    // overload function is a function that have the same name but has different paramater, different return;
+    cout << "x1 = " << x1 << endl;
+    cout << "x2 = " << x2 << endl;
 
     return 0;
 }
-
-    int add(int a, int b){
-        return a + b;
-    }
-    float add(float a, float b){
-        return a + b;
-    }
