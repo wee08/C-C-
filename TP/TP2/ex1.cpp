9f0dfbc23c29aa3b4951ec27b7fdb9759ccc29f5
wee08 <manutnithyasean@gmail.com>
2026-02-28 12:00:34 +0700

    implement sumquare function

--- TP/TP2/ex1.cpp
+++ TP/TP2/ex1.cpp
@@ -4,16 +4,27 @@
 
 using namespace std;
 
-void power(int m, int n){
+int power(int m, int n){
     double result = 1;
     for(int i = 1; i <= n; i++){
         result*=m;
     }
-    cout << result << endl;
+    return result;
 }
 
+int sumSquare(int n){
+    double result = 0;
+    for(int i = 1; i <=n ; i++){
+        result += power(i,2);
+    }
+    return result;
+}
 
 int main(){
-    power(2,7);
+    int result; 
+    result = power(2,7);
+    cout << result << endl;
+    result = sumSquare(10);
+    cout << result << endl;
     return 0;
 }
